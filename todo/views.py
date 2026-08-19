f83585acd60be7d2c9404604683f47ff3d4bed41
mahdi.f <fallahmahdi1383@gmail.com>
2026-08-19 16:00:32 +0330

    show tasks in home page view

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,13 @@
 from django.views.generic.base import TemplateView
+from .models import Task
 
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = Task.objects.all()
+        return context
