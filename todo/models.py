3636fdccdf1905cd327cb78f6d9ac9e9d49bb245
mahdi.f <fallahmahdi1383@gmail.com>
2026-08-19 12:13:55 +0330

    make todo tasks model and admin panel

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+# from accounts.models import Profile
+
 # Create your models here.
+
+
+class Task(models.Model):
+    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=True)
+    complete = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class META:
+        ordering = ["created_date"]
